2ed7b332e590a4959877c54cde4af97ee5908bcf
VitaliiDanyliuk <danyliuk.vitalii@gmail.com>
2022-09-20 12:56:10 +0300

    done

---
+++ Homework JS 1/script.js
@@ -0,0 +1,43 @@
+//Класна робота
+
+//1.
+
+// document.write("<header> </header>");
+
+// document.write("<div> <nav> </nav> <section> </section></div>");
+
+
+// document.write("<footer> </footer>");
+
+
+//2.
+
+// var name = prompt("Введіть Ваше ім'я");
+// document.write(name + "<br />");
+
+
+// var surname = prompt("Ввведіть Ваше прізвище");
+// document.write(surname + "<br />");
+
+// var age = prompt("Введіть Ваш вік");
+// document.write(age + "<br />");
+
+
+//3.
+
+// var num1 = prompt("Ввведіть перше число"),
+// num2 = prompt("Ввведіть друге число"),
+// num3 = prompt("Ввведіть третє число");
+
+// var newNum1 = parseFloat(num1),
+// newNum2 = parseFloat(num2),
+// newNum3 = parseFloat(num3);
+    
+// document.write((newNum1 + newNum2 + newNum3) / 3);
+
+
+
+
+
+//Домашня робота
+
