980a5f29022f5498ecda2edd9239f83001a70c31
VitaliiDanyliuk <danyliuk.vitalii@gmail.com>
2022-12-08 00:17:12 +0200

    done

--- Homework JS 11-12/task3/code/code.js
+++ Homework JS 11-12/task3/code/code.js
@@ -6,21 +6,24 @@
 // переведіть користувача на сторінку https://risovach.ru/upload/2013/03/mem/toni-stark_13447470_big_.jpeg
 // якщо буде помилка , відобразіть її в діві до input
 
-const pattern = /\d{3}-\d{3}-d{2}-d{2}/g;
+
 let input = document.querySelector("#input");
 let div = document.querySelector("#div");
-let form = document.querySelector("#form");
+let btn = document.querySelector("#button");
 
+btn.onclick = function (e) {
+    e.preventDefault();
 
-document.querySelector("#button").onclick = () => { 
-         
-    if (pattern.test(input)) {
-        document.location = "https://www.google.com/search?q=%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8+js&sxsrf=ALiCzsbwMQknwCch6PNf0WYlf-LF3s9f0w:1669490296680&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj9rNuAyMz7AhXomIsKHVMmCIYQ_AUoAXoECAIQAw&biw=1280&bih=609&dpr=1.5#imgrc=TmIunsxZy25myM";
-        form.classList.add("class1");
-        
+    if (!validate(/^\d{3}-\d{3}-\d{2}-\d{2}$/, input.value)) {
+        div.innerHTML = "Помилка! Не вірний формат номера телефону";
+        div.classList.add("class1");
     } else {
-        div.innerHTML = "Не вірний формат номера телефону";
-    }
-}
-        
-    
+        input.style.border = "2px solid green";
+        document.location = "https://www.google.com/search?q=%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8+js&sxsrf=ALiCzsbwMQknwCch6PNf0WYlf-LF3s9f0w:1669490296680&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj9rNuAyMz7AhXomIsKHVMmCIYQ_AUoAXoECAIQAw&biw=1280&bih=609&dpr=1.5#imgrc=TmIunsxZy25myM";
+    };
+};
+
+function validate(patern, value) {
+    return patern.test(value)
+};
+
